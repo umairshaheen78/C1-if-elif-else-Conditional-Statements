158ace873e27eee6ccb961ce8f5cd03988c6f8f9
Umair Shaheen <109942@eesd.org>
2021-01-29 00:18:42 +0000

    h9

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Hello everyone! I am Umair Shaheen, a studen of Python Class #1. I hope you're having fun with Python!
+Hello everyone! I am Umair Shaheen, a studen of Python Class #1. I hope you're having fun with Python! Please do the task below! I hope to see what you put!
+1. Ask the user for their age
+2. If they are younger than 13, tell them they can only watch PG/G movies
+3. If they are older than 13, but younger than 17, they can watch PG-13/PG/G movies
+4. If they are 17 and older, they can watch all the movies
--- main.py
+++ main.py
@@ -42,9 +42,6 @@ infinite (∞) x elif header(s)
 1 x (Maximum) output/result
 Minimum (None)
 
-'''
-
-#Example
 a = 2
 
 if (a == 0):
@@ -58,3 +55,31 @@ elif (a != 2):
 
 else:
   print ("bye")
+
+a = 5000
+num = int(input("Enter a number: "))
+
+if (num > 0):
+  print (" I see that your number is positive!")
+elif (num == 0):
+ print (" You entered 0!")
+else:
+  print ("Its negative!")
+
+'''
+
+# Ask the user for their age
+# If they are younger than 13, tell them they can only watch PG/G movies
+# If they are older than 13, but younger than 17, they can watch PG-13/PG/G movies
+# If they are 17 and older, they can watch all the movies
+
+age = int(input("What is your age?"))
+print("Cool! That's nice to know!")
+if (age < 13):
+ print (" You can only watch PG and G movies!")
+elif (age < 17):
+ print ("You can watch PG-13/PG/G movies!")
+elif (age > 17 ):
+ print ("You can watch any movie!")
+
+
